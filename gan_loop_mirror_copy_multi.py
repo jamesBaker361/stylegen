da31a528ec1a535eed985c520101f1f8f4f97992
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2022-01-04 19:16:55 -0500

    gram matrix but it works

--- gan_loop_mirror_copy_multi.py
+++ gan_loop_mirror_copy_multi.py
@@ -198,7 +198,7 @@ if __name__=='__main__':
         categorical_cross_entropy=tf.keras.losses.CategoricalCrossentropy(from_logits=True,reduction=tf.keras.losses.Reduction.NONE)
         
         def gram_matrix(input_tensor):
-            result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+            result = tf.linalg.einsum('ijc,ijd->cd', input_tensor, input_tensor)
             input_shape = tf.shape(input_tensor)
             num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
             return result/(num_locations)
