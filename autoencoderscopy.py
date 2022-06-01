97c054d5a2bf3067edef10cbaedf1d42bb189ac6
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2022-06-01 17:08:59 -0400

    new gen ae architecture

--- autoencoderscopy.py
+++ autoencoderscopy.py
@@ -18,39 +18,9 @@ from generator import *
 
  #the dim of latent space is dim is 1-D
 
-def get_encoder(inputs,input_dim,residual,
+def get_encoder(inputs,input_dim,residual, attention=False,
     noise_weight=1.0,
     base_flat_noise_dim=0,norm="instance"):
-    '''> The encoder takes an input image and returns a vector of size `base_flat_noise_dim`
-    
-    The encoder is a convolutional neural network that takes an image as input and returns a vector of
-    size `base_flat_noise_dim` if base_flat_noise_dim !=0. Else: The encoder is a series of convolutional layers that downsample the
-    image until it is 2x2x (Dim), where dim is variable
-    
-    Parameters
-    ----------
-    inputs
-        the input tensor
-    input_dim
-        the shape of the input image
-    residual
-        whether to use residual blocks
-    attention
-        whether to use attention in the decoder (depr)
-    noise_weight
-        the amount to scale the noise to add to the encoder output.
-    m, optional
-        the number of layers in the decoder (depr)
-    base_flat_noise_dim, optional
-        the dimension of the flat noise vector.
-    norm, optional
-        "instance", "batch", or "group"
-    
-    Returns
-    -------
-        The encoder is returning a tensor of shape (batch_size, base_flat_noise_dim)
-    
-    '''
         
     normalization=InstanceNormalization
     if norm == "batch":
@@ -74,17 +44,13 @@ def get_encoder(inputs,input_dim,residual,
     if residual==True:
         x = ResNextBlock(kernel_size=(4, 4))(x)
     
-    while x.shape[-1]<64:
-        channels = x.shape[-1] *2
-        x=block(x, channels)
     
-    while x.shape[-1]>64:
-        channels = x.shape[-1] //2
-        x=block(x, channels)
-    while x.shape[-2] >2:
+    while x.shape[-2] >16:
+        if x.shape[-2]<64 and attention==True:
+            x=attn_block(x)
         channels = x.shape[-1]
-        if residual==True:
-            x = ResNextBlock(kernel_size=(4, 4))(x)
+        if channels<64:
+            channels=channels*2
         #print(x.shape)
         x = layers.Conv2D(channels, (4, 4), (2, 2), padding='same')(x)
         x=normalization()(x)
@@ -92,10 +58,23 @@ def get_encoder(inputs,input_dim,residual,
         x=layers.LeakyReLU()(x)
         if residual==True:
             x = ResNextBlock(kernel_size=(4, 4))(x)
-        x=layers.Conv2D(channels,(2,2),padding='same')(x)
+    while x.shape[-2]>2:
+        if x.shape[-2]<64 and attention==True:
+            x=attn_block(x)
+        if channels<64:
+            channels=channels*2
+        x = layers.Conv2D(channels, (3, 3), (1, 1))(x)
         x=normalization()(x)
         ##x=layers.Dropout(.2)(x)
         x=layers.LeakyReLU()(x)
+        if residual==True:
+            x = ResNextBlock(kernel_size=(4, 4))(x)
+    while x.shape[-1]>64:
+        channels = x.shape[-1]//2
+        x=layer.Conv2D(channels,(1,1),(1,1))(x)
+        x=normalization()(x)
+        x=layers.LeakyReLU()(x)
+
 
     if base_flat_noise_dim>0:
         x=layers.Flatten()(x)
@@ -103,27 +82,6 @@ def get_encoder(inputs,input_dim,residual,
     return x
 
 def make_decoder(input_dim,residual,attention,flat_latent_dim=0,norm="instance"):
-    '''It takes an input, and then applies a series of convolutions and transposed convolutions to it, with
-    some residual blocks and attention blocks thrown in
-    
-    Parameters
-    ----------
-    input_dim
-        the shape of the input image
-    residual
-        Whether to use residual blocks in the decoder.
-    attention
-        whether to use attention blocks
-    flat_latent_dim, optional
-        if you flat_latent_dim !=0, then it must be reshaped to be (2,2,64)
-    norm, optional
-        "instance", "batch", or "group"
-    
-    Returns
-    -------
-        A model with the input and output layers defined.
-    
-    '''
     normalization=InstanceNormalization
     if norm == "batch":
         normalization=BatchNormalization
@@ -147,7 +105,7 @@ def make_decoder(input_dim,residual,attention,flat_latent_dim=0,norm="instance")
         channels = max(x.shape[-1]//2,32)
         if residual==True:
             x = ResNextBlock(kernel_size=(4, 4))(x)
-        x = layers.Conv2DTranspose(channels, (4, 4), (2, 2),padding='same')(x)
+        x = layers.Conv2DTranspose(channels, (3, 3), (2, 2),padding='same')(x)
         x=normalization()(x)
         ##x=layers.Dropout(.2)(x)
         x=layers.LeakyReLU()(x)
@@ -155,50 +113,21 @@ def make_decoder(input_dim,residual,attention,flat_latent_dim=0,norm="instance")
             x = ResNextBlock(kernel_size=(4, 4))(x)
         if x.shape[-2]<64 and attention==True:
             x=attn_block(x)
-    x = layers.Conv2D(x.shape[-1], (8, 8), (1, 1),padding='same')(x)
+    x = layers.Conv2D(16, (5, 5), (1, 1),padding='same')(x)
     x = GroupNormalization(groups=x.shape[-1] // 4)(x)
     x = tk.activations.swish(x)
-    x = layers.Conv2D(8, (8, 8), (1, 1),padding='same')(x)
+    x = layers.Conv2D(8, (3, 3), (1, 1),padding='same')(x)
     x = GroupNormalization(groups=x.shape[-1] // 4)(x)
     x = tk.activations.swish(x)
-    x = layers.Conv2D(4, (4, 4), (1, 1),padding='same')(x)
+    x = layers.Conv2D(4, (1, 1), (1, 1),padding='same')(x)
     x = GroupNormalization(groups=x.shape[-1] // 4)(x)
     x = tk.activations.swish(x)
-    x = layers.Conv2D(3, (4, 4), (1, 1),padding='same')(x)
+    x = layers.Conv2D(3, (1, 1), (1, 1),padding='same')(x)
     x=layers.Activation('sigmoid')(x)
     x=Rescaling(255,name='img_output')(x)
     return tk.Model(inputs=inputs,outputs=x,name='decoder')
 
 def aegen(block,base_flat_noise_dim=0,residual=True,attention=True,output_blocks=[],art_styles=[],norm="instance",noise_weight=1.0,batch_size=1):
-    '''`aegen` takes in an image, encodes it, adds some noise, decodes it, and then compares the decoded
-    image to the original image
-    
-    Parameters
-    ----------
-    block
-        the block of the vgg19 model to use as the output.
-    base_flat_noise_dim, optional
-        The dimension of the latent space that is not used for the art style.
-    residual, optional
-        Whether to use residual blocks in the encoder and decoder
-    attention, optional
-        Whether to use attention in the encoder and decoder
-    output_blocks
-        the layers of the vgg19 model that you want to use as outputs.
-    art_styles
-        a list of strings, each string is the name of a style.
-    norm, optional
-        "instance" or "batch"
-    noise_weight, optional
-        the amount to scale the noise to add to the encoder output.
-    batch_size, optional
-        batch size
-    
-    Returns
-    -------
-        The model is being returned.
-    
-    '''
     input_shape=input_shape_dict[block]
     inputs = tk.Input(shape=input_shape)
     flat_latent_dim=base_flat_noise_dim+len(art_styles)
